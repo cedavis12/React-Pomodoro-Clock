0b734446bebcf41b131666a4bc0b759e2151bcaf
Courtney Davis <courtneydavis@The-Davis-iMac.local>
2019-12-15 11:47:01 -0500

    Project set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Fragment>
+      <nav className='h1 navbar text-white mx-auto display-3' id='header'>
+        Pomodoro Clock
+      </nav>
+      <div className='container text-center'>
+        <div id='time'>
+          <h2 id='session-label' class='mt-5 display-4'>
+            Session Length
+          </h2>
+          <a
+            href='#'
+            className='btn btn-outline-light btn-lg m-3'
+            id='sessionDecrement'
+          >
+            -
+          </a>
+          <span id='sessionLength' class='lead'>
+            25
+          </span>
+          <a
+            href='#'
+            className='btn btn-outline-light btn-lg m-3'
+            id='sessionIncrement'
+          >
+            +5
+          </a>
+          <br />
+          <a href='#' className='btn btn-warning text-white btn-lg' id='reset'>
+            Reset Timer
+          </a>
+        </div>
+        <div id='break'>
+          <h2 id='break-label' className='mt-5 display-4'>
+            Break Length
+          </h2>
+          <a
+            href='#'
+            className='btn btn-outline-light btn-lg m-3'
+            id='subtractFiveBreak'
+          >
+            -5
+          </a>
+          <span id='break-length' className='lead'>
+            5
+          </span>
+          <a
+            href='#'
+            className='btn btn-outline-light btn-lg m-3'
+            id='addFiveBreak'
+          >
+            +5
+          </a>
+        </div>
+        <a href='#' className='btn btn-success btn-lg' id='start'>
+          Start
         </a>
-      </header>
-    </div>
+      </div>
+      <nav
+        className='h1 navbar text-white fixed-bottom my-0 mx-auto'
+        id='footer'
+      >
+        Coded by Courtney Davis
+      </nav>
+    </Fragment>
   );
 }
 
